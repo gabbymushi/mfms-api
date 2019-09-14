f59694707040288695114727632bc4beee72a698
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2019-09-14 14:07:15 +0300

    store heir details

--- app/Http/Controllers/HeirController.php
+++ app/Http/Controllers/HeirController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Heir;
 
 class HeirController extends Controller
 {
@@ -34,7 +35,22 @@ class HeirController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $model = new Heir();
+        $model->first_name = $request->input('first_name');
+        $model->middle_name = $request->input('middle_name');
+        $model->last_name = $request->input('last_name');
+        $model->gender = $request->input('gender');
+        $model->birthday = $request->input('birthday');
+        $model->phone_no = $request->input('phone_no');
+        $model->phone_no_2 = $request->input('phone_no_2');
+        $model->email = $request->input('email');
+        $model->address = $request->input('address');
+        $model->residence = $request->input('residence');
+        $model->member_id = $request->input('member_id');
+        $model->business = $request->input('business');
+        $model->relation = $request->input('relation');
+        $model->save();
+        return $model;
     }
 
     /**
